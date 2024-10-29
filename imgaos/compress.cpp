26fe7bf6c8e44717aba2a35817e5bcf27c76bc3e
Olaya Martinez <olayamf03@gmail.com>
2024-10-29 18:35:42 +0100

    compress

--- imgaos/compress.cpp
+++ imgaos/compress.cpp
@@ -10,28 +10,31 @@
 CCompress::CCompress(const std::string& outputFilePath) : outputFilePath(outputFilePath) {}
 
 //{
-// void CCompress::compress(const std::vector<Pixel>& imagePixels) {
-    //std::ifstream input(params.input_file, std::ios::binary);
-    //if (!input.is_open()) {
-      //std::cerr << "Error opening file " << params.input_file << std::endl;
-      //return;
-    /}
+void CCompress::compress(const std::vector<Pixel<uint8_t>(inputPixels),metadata.maxColorValue, args.extra[0] {
+    //initialize values of width, height and colormax
+    maxColorValue = metadata.maxColorValue;
+    int width, height, maxColorValue = 0;
+    openin
+
 //}
 
 void getpixels(const inputFile) {
-  for (std::size_t i = 0; i < dataSize; i++) {
-    image[i].r = readBinary<T>(inputFile);
-    data[i].g = readBinary<T>(inputFile);
-    data[i].b = readBinary<T>(inputFile);
-  }
+
+  int const limitvalue = 255;
+  for (std::size_t i = 0; i < pixels.size; i++) {
+    if (maxColorValue < limitvalue) {
+    	outputFile[i].r = binary::readBinary<uint8_t>(inputFile);
+    	outputFile[i].g = binary::readBinary<uint8_t>(inputFile);
+    	outputFile[i].b = binary::readBinary<uint8_t>(inputFile);
+    }else{
+   		outputFile[i].r = binary::readBinary<uint16_t>(inputFile);
+        outputFile[i].g = binary::readBinary<uint16_t>(inputFile);
+        outputFile[i].b = binary::readBinary<uint16_t>(inputFile);
+  	}
   return data;
-}
+
+
 void CCompress::writeCompressedData(const std::vector<uint8_t>& compressedData) {
-    std::ofstream outFile(outputFilePath, std::ios::binary);
-    if (!outFile) {
-        std::cerr << "Error opening output file: " << outputFilePath << std::endl;
-        return;
-    }
 
     outFile.write(reinterpret_cast<const char*>(compressedData.data()), compressedData.size());
     outFile.close();
