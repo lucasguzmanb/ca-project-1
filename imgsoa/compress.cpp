3812b2458cab2914ad60584fa08dc171c8f8b420
Olaya Martinez <olayamf03@gmail.com>
2024-10-30 02:01:56 +0100

    compress

--- imgsoa/compress.cpp
+++ imgsoa/compress.cpp
@@ -1,1 +1,14 @@
 #include "compress.hpp"
+
+
+void compress();
+
+void getpixels(&inputImage);
+
+void writetable(std::map<std::truple<iunt16_t, uint16_t, uint16_t>&colorMap);
+
+std::map<std::truple<iunt16_t, uint16_t, uint16_t> getcolours(%inputImage);
+
+void writepixels(inputImage, std::map<std::truple<iunt16_t, uint16_t, uint16_t>&colorMap);
+
+#endif //COMPRESS_HPP
--- imgsoa/compress.hpp
+++ imgsoa/compress.hpp
@@ -1,4 +1,15 @@
 #ifndef COMPRESS_HPP
 #define COMPRESS_HPP
 
+void compress();
+
+void getpixels(&inputImage);
+
+void writetable(std::map<std::truple<iunt16_t, uint16_t, uint16_t>&colorMap);
+
+std::map<std::truple<iunt16_t, uint16_t, uint16_t> getcolours(%inputImage);
+
+void writepixels(inputImage, std::map<std::truple<iunt16_t, uint16_t, uint16_t>&colorMap);
+
+
 #endif //COMPRESS_HPP
