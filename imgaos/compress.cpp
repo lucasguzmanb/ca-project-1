4ddcb31ad8a06955805b48251c34f38cc8950ddd
Olaya Martinez <olayamf03@gmail.com>
2024-10-30 01:41:42 +0100

    compress

--- imgaos/compress.cpp
+++ imgaos/compress.cpp
@@ -10,15 +10,17 @@
 CCompress::CCompress(const std::string& outputFilePath) : outputFilePath(outputFilePath) {}
 
 //{
-void CCompress::compress(const std::vector<Pixel<uint8_t>(inputPixels),metadata.maxColorValue, args.extra[0] {
+void CCompress::compress(const std::vector<Pixel<uint8_t>(inputPixels),metadata.maxColorValue, args.extra[0]) {
     //initialize values of width, height and colormax
     maxColorValue = metadata.maxColorValue;
-    int width, height, maxColorValue = 0;
-    openin
+    getpixels(inputPixels);
+    std::map<Color, uint16_t>colorMap = getcolours(inputPixels, n_colors);
+    write_data(outputFile, colorMap, maxColorValue);
 
-//}
 
-void getpixels(const inputFile) {
+}
+
+void getpixels(const inputFile){
 
   int const limitvalue = 255;
   for (std::size_t i = 0; i < pixels.size; i++) {
@@ -32,10 +34,41 @@ void getpixels(const inputFile) {
         outputFile[i].b = binary::readBinary<uint16_t>(inputFile);
   	}
   return data;
-
-
-void CCompress::writeCompressedData(const std::vector<uint8_t>& compressedData) {
-
-    outFile.write(reinterpret_cast<const char*>(compressedData.data()), compressedData.size());
-    outFile.close();
+  }
 }
+
+std:: map<Color, uint16_> getcolours(const std::vector<Pixel<uint8_t>(inputPixels),metadata.maxColorValue, args.extra[0]) {
+  std::map<Color, uint16_> colours;
+
+}
+void CCompress::writeCompressedData(outputFile, const std::map<Color, uint16_>& colorMap, n_colors) {
+	int limitvalue = 255;
+    int limitvalue2 = 65535;
+    for (coonst [color: colorMap]{
+    	if(maxColorValue < limitvalue) {
+        	const r = static_cast<uint8_t>(color.r);
+            const g = static_cast<uint8_t>(color.g);
+            const b = static_cast<uint8_t>(color.b);
+            binary::writeBinary(outputFile, r);
+            binary::writeBinary(outputFile, g);
+            binary::writeBinary(outputFile, b);
+        }else{
+         	binary::writeBinary(outputFile, color.r);
+            binary::writeBinary(outputFile, color.g);
+            binary::writeBinary(outputFile, color.b);
+        }
+    }
+    for (std::size_t i = 0; i < pixels.size; i++) {
+    	if (n_colours <= limitvalue) {
+        	index = static_cast<uint8_t>(colorMap[i];
+			binary::writeBinary(outputFile, index);
+    	}else if ((n_colours >=limit1+1) && (n_colours <= limitvalue2)) {
+        	uint16_t index =colorMap[i];
+            binary::writeBinary(outputFile, index);
+        }else{
+          index = static_cast<uint32_t>(colorMap[i]);
+          binary::writeBinary(outputFile, index);
+        }
+    }
+}
+
