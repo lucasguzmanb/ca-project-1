0ca74e11e1ff4a8f59a48cf4c161b5fe34d98f0b
Lucas Guzman <lucas_guzman@hotmail.es>
2024-11-02 11:55:50 +0100

    Rewrite AOStoBinary and BinaryToAOS functions to read all data at once so it goes much faster

--- imgaos/imageaos.cpp
+++ imgaos/imageaos.cpp
@@ -1,14 +1,1 @@
 #include "imageaos.hpp"
-
-void writeBinaryData(
-    std::ofstream & outputFile,
-    std::variant<std::vector<Pixel<uint8_t>>, std::vector<Pixel<uint16_t>>> const & outputPixels) {
-  if (std::holds_alternative<std::vector<Pixel<uint8_t>>>(outputPixels)) {
-    AOSToBinary<uint8_t>(outputFile, std::get<std::vector<Pixel<uint8_t>>>(outputPixels));
-  } else if (std::holds_alternative<std::vector<Pixel<uint16_t>>>(outputPixels)) {
-    AOSToBinary<uint16_t>(outputFile, std::get<std::vector<Pixel<uint16_t>>>(outputPixels));
-  } else {
-    std::cerr << "Error: unknown variant type\n";
-    exit(-1);
-  }
-}
--- imgaos/imageaos.hpp
+++ imgaos/imageaos.hpp
@@ -20,10 +20,6 @@ struct Pixel {
     }
 };
 
-void writeBinaryData(
-    std::ofstream & outputFile,
-    std::variant<std::vector<Pixel<uint8_t>>, std::vector<Pixel<uint16_t>>> const & outputPixels);
-
 template <typename T>
 struct Pixel_map {
     std::size_t operator()(Pixel<T> const & color) const {
@@ -32,24 +28,42 @@ struct Pixel_map {
 };
 
 template <typename T>
-std::vector<Pixel<T>> binaryToAOS(std::ifstream & inputFile, int width, int height) {
-  auto dataSize = static_cast<std::size_t>(width * height);
-  std::vector<Pixel<T>> data(dataSize);
-  for (std::size_t i = 0; i < dataSize; ++i) {
-    data[i].r = readBinary<T>(inputFile);
-    data[i].g = readBinary<T>(inputFile);
-    data[i].b = readBinary<T>(inputFile);
+std::vector<Pixel<T>> binaryToAOS(std::ifstream & inputFile, int const width, int const height) {
+  // Create a vector of raw data to store the binary data and read everything at once
+  auto dataSize = static_cast<std::size_t>(width * height * 3);
+  std::vector<T> tempData(dataSize);
+
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  inputFile.read(reinterpret_cast<char *>(tempData.data()),
+                 static_cast<std::streamsize>(dataSize * sizeof(T)));
+
+  std::vector<Pixel<T>> data(static_cast<std::size_t>(width * height));
+
+  // Convert the raw data to AOS
+  for (std::size_t i = 0, j = 0; i < data.size(); ++i, j += 3) {
+    data[i].r = tempData[j];
+    data[i].g = tempData[j + 1];
+    data[i].b = tempData[j + 2];
   }
+
   return data;
 }
 
 template <typename T>
 void AOSToBinary(std::ofstream & outputFile, std::vector<Pixel<T>> const & data) {
-  for (auto pixel : data) {
-    writeBinary(outputFile, pixel.r);
-    writeBinary(outputFile, pixel.g);
-    writeBinary(outputFile, pixel.b);
+  // Convert the AOS data to raw data
+  std::vector<T> tempData(data.size() * 3);
+
+  for (std::size_t i = 0, j = 0; i < data.size(); ++i, j += 3) {
+    tempData[j]     = data[i].r;
+    tempData[j + 1] = data[i].g;
+    tempData[j + 2] = data[i].b;
   }
+
+  // Write the raw data to the binary file
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  outputFile.write(reinterpret_cast<char const *>(tempData.data()),
+                   static_cast<std::streamsize>(tempData.size() * sizeof(T)));
 }
 
 #endif  // IMAGEAOS_HPP
