b72793aa50dcb67ad57356f3cc06eeee4404d8a5
Lucas Guzman <lucas_guzman@hotmail.es>
2024-11-07 17:56:47 +0100

    some tiny optimizations to maxlevel in both AOS and SOA

--- imgaos/maxlevel.hpp
+++ imgaos/maxlevel.hpp
@@ -3,48 +3,24 @@
 
 #include "imageaos.hpp"
 
-#include <cstdint>
-#include <variant>
 #include <vector>
 
 template <typename T, typename U>
-std::vector<Pixel<U>> maxlevel(std::vector<Pixel<T>> inputImage, int const oldLevel, int const newLevel) {
-    std::vector<Pixel<U>> outputImage(inputImage.size());
-        for (size_t i = 0; i < inputImage.size(); ++i) {
-                outputImage[i].r = static_cast<U>((inputImage[i].r * newLevel + oldLevel / 2) / oldLevel);
-                outputImage[i].g = static_cast<U>((inputImage[i].g * newLevel + oldLevel / 2) / oldLevel);
-                outputImage[i].b = static_cast<U>((inputImage[i].b * newLevel + oldLevel / 2) / oldLevel);
-        }
-  return outputImage;
-}
+std::vector<Pixel<U>> maxlevel(std::vector<Pixel<T>> inputImage, int const oldLevel,
+                               int const newLevel) {
+  std::vector<Pixel<U>> outputImage(inputImage.size());
+  double const oldLevelHalf    = oldLevel / 2.0;
+  double const oldLevelInverse = 1.0 / oldLevel;
 
-template <typename T>
-std::vector<Pixel<uint8_t>> maxlevel8(std::vector<Pixel<T>> const & inputImage, int const oldLevel,
-                                      int const newLevel) {
-  std::vector<Pixel<uint8_t>> outputImage(inputImage.size());
   for (size_t i = 0; i < inputImage.size(); ++i) {
     outputImage[i].r =
-        static_cast<uint8_t>((inputImage[i].r * newLevel + oldLevel / 2) / oldLevel);
+        static_cast<U>((inputImage[i].r * newLevel + oldLevelHalf) * oldLevelInverse);
     outputImage[i].g =
-        static_cast<uint8_t>((inputImage[i].g * newLevel + oldLevel / 2) / oldLevel);
+        static_cast<U>((inputImage[i].g * newLevel + oldLevelHalf) * oldLevelInverse);
     outputImage[i].b =
-        static_cast<uint8_t>((inputImage[i].b * newLevel + oldLevel / 2) / oldLevel);
+        static_cast<U>((inputImage[i].b * newLevel + oldLevelHalf) * oldLevelInverse);
   }
-  return outputImage;
-}
 
-template <typename T>
-std::vector<Pixel<uint16_t>> maxlevel16(std::vector<Pixel<T>> const & inputImage,
-                                        int const oldLevel, int const newLevel) {
-  std::vector<Pixel<uint16_t>> outputImage(inputImage.size());
-  for (size_t i = 0; i < inputImage.size(); ++i) {
-    outputImage[i].r =
-        static_cast<uint16_t>((inputImage[i].r * newLevel + oldLevel / 2) / oldLevel);
-    outputImage[i].g =
-        static_cast<uint16_t>((inputImage[i].g * newLevel + oldLevel / 2) / oldLevel);
-    outputImage[i].b =
-        static_cast<uint16_t>((inputImage[i].b * newLevel + oldLevel / 2) / oldLevel);
-  }
   return outputImage;
 }
 
--- imgsoa/maxlevel.hpp
+++ imgsoa/maxlevel.hpp
@@ -3,46 +3,18 @@
 
 #include "imagesoa.hpp"
 
-#include <cstdint>
-#include <variant>
-
 template <typename T, typename U>
 ImageSOA<U> maxlevel(ImageSOA<T> inputImage, int const oldLevel, int const newLevel) {
-        ImageSOA<U> outputImage(inputImage.r.size());
-        for (size_t i = 0; i < inputImage.r.size(); ++i) {
-        outputImage.r[i] = static_cast<U>((inputImage.r[i] * newLevel + oldLevel / 2) / oldLevel);
-        outputImage.g[i] = static_cast<U>((inputImage.g[i] * newLevel + oldLevel / 2) / oldLevel);
-        outputImage.b[i] = static_cast<U>((inputImage.b[i] * newLevel + oldLevel / 2) / oldLevel);
-        }
-        return outputImage;
-}
-
-template <typename T>
-ImageSOA<uint8_t> maxlevel8(ImageSOA<T> const & inputImage, int const oldLevel,
-                            int const newLevel) {
-  ImageSOA<uint8_t> outputImage(inputImage.r.size());
+  ImageSOA<U> outputImage(inputImage.r.size());
+  double const oldLevelHalf    = oldLevel / 2.0;
+  double const oldLevelInverse = 1.0 / oldLevel;
   for (size_t i = 0; i < inputImage.r.size(); ++i) {
     outputImage.r[i] =
-        static_cast<uint8_t>((inputImage.r[i] * newLevel + oldLevel / 2) / oldLevel);
+        static_cast<U>((inputImage.r[i] * newLevel + oldLevelHalf) * oldLevelInverse);
     outputImage.g[i] =
-        static_cast<uint8_t>((inputImage.g[i] * newLevel + oldLevel / 2) / oldLevel);
+        static_cast<U>((inputImage.g[i] * newLevel + oldLevelHalf) * oldLevelInverse);
     outputImage.b[i] =
-        static_cast<uint8_t>((inputImage.b[i] * newLevel + oldLevel / 2) / oldLevel);
-  }
-  return outputImage;
-}
-
-template <typename T>
-ImageSOA<uint16_t> maxlevel16(ImageSOA<T> const & inputImage, int const oldLevel,
-                              int const newLevel) {
-  ImageSOA<uint16_t> outputImage(inputImage.r.size());
-  for (size_t i = 0; i < inputImage.r.size(); ++i) {
-    outputImage.r[i] =
-        static_cast<uint16_t>((inputImage.r[i] * newLevel + oldLevel / 2)/ oldLevel);
-    outputImage.g[i] =
-        static_cast<uint16_t>((inputImage.g[i] * newLevel + oldLevel / 2) / oldLevel);
-    outputImage.b[i] =
-        static_cast<uint16_t>((inputImage.b[i] * newLevel + oldLevel / 2) / oldLevel);
+        static_cast<U>((inputImage.b[i] * newLevel + oldLevelHalf) * oldLevelInverse);
   }
   return outputImage;
 }
