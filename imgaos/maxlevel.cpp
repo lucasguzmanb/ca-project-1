b29a2b361afff531c0a5c6ad45ec1189fa10d430
Lucas Guzman <lucas_guzman@hotmail.es>
2024-11-11 18:54:41 +0100

    minor change in maxlevel.cpp

--- imgaos/maxlevel.cpp
+++ imgaos/maxlevel.cpp
@@ -6,7 +6,7 @@ template <typename T, typename U>
 std::vector<Pixel<U>> maxlevel(std::vector<Pixel<T>> inputImage, int const oldLevel,
                                int const newLevel) {
   std::vector<Pixel<U>> outputImage(inputImage.size());
-  double const transformation = newLevel * (1.0 / oldLevel);
+  double const transformation = static_cast<double>(newLevel) / oldLevel;
   // Convert each pixel to the new level using a formula to round the result
   for (size_t i = 0; i < inputImage.size(); ++i) {
     outputImage[i].r =
