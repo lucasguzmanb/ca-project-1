a74f5a68661b398d71de157b76706aa77a0280cf
Venka03 <venka03@knu.ua>
2024-11-14 02:30:38 +0100

    read and write 16 bits soa

--- imgsoa/imagesoa.hpp
+++ imgsoa/imagesoa.hpp
@@ -57,9 +57,16 @@ ImageSOA<T> binaryToSOA(std::ifstream & inputFile, int const width, int const he
 
   // Convert the raw data to SOA
   for (std::size_t i = 0, j = 0; i < data.r.size(); ++i, j += 3) {
-    data.r[i] = tempData[j];
-    data.g[i] = tempData[j + 1];
-    data.b[i] = tempData[j + 2];
+    if (sizeof(T) == 2){
+      data.r[i] = static_cast<T>((tempData[j] << 8) | (tempData[j] >> 8));
+      data.g[i] = static_cast<T>((tempData[j+1] << 8) | (tempData[j+1] >> 8));
+      data.b[i] = static_cast<T>((tempData[j+2] << 8) | (tempData[j+2] >> 8));
+    }
+    else {
+      data.r[i] = tempData[j];
+      data.g[i] = tempData[j + 1];
+      data.b[i] = tempData[j + 2];
+    }
   }
 
   return data;
@@ -71,9 +78,16 @@ void SOAToBinary(std::ofstream & outputFile, ImageSOA<T> const & data) {
   std::vector<T> tempData(data.r.size() * 3);
 
   for (std::size_t i = 0, j = 0; i < data.r.size(); ++i, j += 3) {
-    tempData[j]     = data.r[i];
-    tempData[j + 1] = data.g[i];
-    tempData[j + 2] = data.b[i];
+    if (sizeof(T) == 2){
+      tempData[j]     = static_cast<T>((data.r[i] << 8) | (data.r[i] >> 8));
+      tempData[j + 1] = static_cast<T>((data.g[i] << 8) | (data.g[i] >> 8));
+      tempData[j + 2] = static_cast<T>((data.b[i] << 8) | (data.b[i] >> 8));
+    }
+    else {
+      tempData[j]     = data.r[i];
+      tempData[j + 1] = data.g[i];
+      tempData[j + 2] = data.b[i];
+    }
   }
 
   // Write the raw data to the output file
